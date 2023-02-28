5c117ff70908e28b99b9ec146ebedb44d3bc617b
PrecieuxMudibu <oceangpmb@gmail.com>
2023-02-28 01:05:38 +0100

    feat: Add posibbility to return one or all projects

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -10,6 +10,16 @@ const {
   GraphQLList,
 } = graphql;
 
+const ProjectType = new GraphQLObjectType({
+  name: "Project",
+  fields: () => ({
+    id: { type: GraphQLID },
+    name: { type: GraphQLString },
+    description: { type: GraphQLString },
+    status: { type: GraphQLString },
+  }),
+});
+
 const ClientType = new GraphQLObjectType({
   name: "Client",
   fields: () => ({
@@ -23,6 +33,19 @@ const ClientType = new GraphQLObjectType({
 const RootQuery = new GraphQLObjectType({
   name: "RootQueryType",
   fields: {
+    projects: {
+      type: new GraphQLList(ProjectType),
+      resolve(parents, args) {
+        return projects;
+      },
+    },
+    project: {
+      type: ProjectType,
+      args: { id: { type: GraphQLID } },
+      resolve(parents, args) {
+        return projects.find((project) => project.id === args.id);
+      },
+    },
     clients: {
       type: new GraphQLList(ClientType),
       resolve(parents, args) {
